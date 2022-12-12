fff16805519a0687cb474ebc0a9c7a72c87a0c92
Canqu1 <92505706+Canqu1@users.noreply.github.com>
2022-12-12 23:50:24 +0700

    First commit

---
+++ Project_Intern/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Intern.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
