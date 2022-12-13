866601e8ec1e7f85bea74af7981b1a650e19b01f
Canqu1 <92505706+Canqu1@users.noreply.github.com>
2022-12-13 16:43:32 +0700

    Update Shop

--- Project_Intern/Controllers/ShopController.cs
+++ Project_Intern/Controllers/ShopController.cs
@@ -14,10 +14,14 @@ namespace Project_Intern.Controllers
         // GET: Shop
         public ActionResult Index()
         {
+            List<products> product = db.products.ToList();
+            ViewBag.Product = product;
             return View();
-        }
+        }   
         public ActionResult Product()
         {
+            List<products> product = db.products.ToList();
+            ViewBag.Product = product;
             return View();
         }
     }
