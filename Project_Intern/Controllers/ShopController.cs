0a0a266b3ad64e2bb46c74690b497a36d0b50312
Canqu1 <92505706+Canqu1@users.noreply.github.com>
2022-12-14 11:41:46 +0700

    Update Phân trang

--- Project_Intern/Controllers/ShopController.cs
+++ Project_Intern/Controllers/ShopController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Project_Intern.Models;
+using PagedList;
 
 
 namespace Project_Intern.Controllers
@@ -12,14 +13,40 @@ namespace Project_Intern.Controllers
     {
         FruitShopEntities db = new FruitShopEntities();
         // GET: Shop
-        public ActionResult Index()
+        public ActionResult Index(int ? page)
         {
-            List<products> product = db.products.ToList();
+            if (page == null) page = 1;
+            var links = (from l in db.products
+                         select l).OrderBy(x => x.product_id); 
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+
+            var product = db.products.ToList();
             ViewBag.Product = product;
-            return View();
+           var category = db.categories.ToList();
+            ViewBag.Category = category;
+            return View(links.ToPagedList(pageNumber, pageSize));
         }   
-        public ActionResult Product()
+        [HttpPost]
+        public ActionResult Index(string cateid)
+        {        
+
+                ViewBag.selectedid = Int32.Parse(cateid);
+                var category = db.categories.ToList();
+                ViewBag.Categories = category;
+
+                var products = db.products.ToList();
+                if (cateid != "0")
+                {
+                    products = db.products
+                        .Where(p => p.category_id == Int32.Parse(cateid))
+                        .ToList();
+                }
+                return View(products);       
+        }
+        public ActionResult Product( int Page=1)
         {
+            int IrtemsPerPage;
             List<products> product = db.products.ToList();
             ViewBag.Product = product;
             return View();
